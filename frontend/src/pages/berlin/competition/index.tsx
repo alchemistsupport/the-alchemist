c51bfdc3aa99201ce2869204e930a2c34a3f05da
Ahmed Ali <thunderstruck772@gmail.com>
2024-11-21 11:40:58 +0200

    fixed the fetching query bugs

--- frontend/src/pages/berlin/competition/index.tsx
+++ frontend/src/pages/berlin/competition/index.tsx
@@ -38,28 +38,28 @@ export const getStaticProps: GetStaticProps = async context => {
   const [footerRes, headerRes, screenmenuRes, homepageRes] = await Promise.all([
     fetchAPI('/footer', {
       populate: {
-        Logo_black: '*',
-        Logo_gold: '*',
+        Logo_black: { populate: '*' },
+        Logo_gold: { populate: '*' },
       },
       locale: context.locale,
     }),
     fetchAPI('/header', {
       populate: {
-        first: '*',
-        second: '*',
+        first: { populate: '*' },
+        second: { populate: '*' },
       },
       locale: context.locale,
     }),
     fetchAPI('/screenmenu', {
       populate: {
-        image: '*',
-        navigation: '*',
+        image: { populate: '*' },
+        navigation: { populate: '*' },
       },
       locale: context.locale,
     }),
     fetchAPI('/homepage', {
       populate: {
-        sign_up: '*',
+        sign_up: { populate: '*' },
       },
       locale: context.locale,
     }),
