df7c81a6136ece036643e27048cb51c638157d45
Ahmed Ali <thunderstruck772@gmail.com>
2025-04-11 10:59:00 +0200

    Adjusted map-image function to process backend local images

--- frontend/src/shared/utils/map-images.ts
+++ frontend/src/shared/utils/map-images.ts
@@ -7,6 +7,6 @@ export type ImageType = {
 
 export const mapImages = (array = []): ImageType[] =>
   array.map((item: ApiHomepageHomepage['attributes']['gallery']) => ({
-    src: item.url,
+    src: item.url.startsWith('/uploads') ? `https://server.thealchemist.de${item.url}` : item.url,
     alt: item.name,
   }));
